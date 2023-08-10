3c11dba1039cf2b0f7889cd96f2942f4f8164906
MatheusLauri <matheuslauri2509@gmail.com>
2023-08-10 13:39:48 -0300

    coloquei função map e função pra puxar os filmes

--- src/pagina-principal/index.js
+++ src/pagina-principal/index.js
@@ -1,9 +1,18 @@
-
-
-
 import './index.scss';
-
+import { useState } from 'react';
+import axios from 'axios';
 export default function Netflix() {
+     const [filme, setFilme] = useState('');
+     const [catalogo, setCatalogo] = useState([])
+
+        async function Buscar() {
+        let url = `https://www.omdbapi.com/?apikey=37e41b24&s=${filme}`;
+
+        let resp = await axios.get(url);
+        setCatalogo(resp.data.Search);
+    }
+
+
 
     return (
         <div className="main-netflix">
@@ -19,13 +28,19 @@ export default function Netflix() {
                 </div>
 
                 <div className="pesquisa-cabecalho">
-                    <input type='text' placeholder='Pesquise pelo titulo'></input>
-                    <img src="./assets/img/icon-buscar.png" className='buscar' alt="buscar" />
+                    <input type='text' placeholder='Pesquise pelo titulo' value={filme} onChange={(e) => setFilme(e.target.value)}/>
+                    <img src="./assets/img/icon-buscar.png" className='buscar' alt="buscar" onClick={() => Buscar()}/>
                     <p>Bruno</p>
                     <img src="./assets/img/sino.png" className='sino' alt="sino" />
                     <img src="./assets/img/fotinha.png" className='foto-pessoa' alt="fotinha" />
                     <img src="./assets/img/seta.png" alt="seta para baixo" />
                 </div>
+            </div>
+
+            <div>
+                {catalogo.map(item => (
+                    <img src={item.Poster}/>
+                ))}
             </div>
         </div>
     )
